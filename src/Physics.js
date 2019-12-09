69789d7a479f16b3c463cbd572482e1a2892a2de
kkr0913 <40028293+kkr0913@users.noreply.github.com>
2019-12-09 02:04:58 -0500

    Minor fix

--- src/Physics.js
+++ src/Physics.js
@@ -5,11 +5,11 @@
 
 function Physics(initialVec) {
 	this.mass = 1;                                // mass
-	this.g = -6;                                  // gravitational acceleration
+	this.g = -0.006;                              // gravitational acceleration
 	this.mu = 0.05;                               // friction coefficient
-  this.pos = initialVec;                        // position (set to initial position)
-  this.vel = createVector(0, 0);                // velocity
-  this.acc = createVector(0, 0);                // acceleration
+	this.pos = initialVec;                        // position (set to initial position)
+	this.vel = createVector(0, 0);                // velocity
+	this.acc = createVector(0, 0);                // acceleration
 	this.ratio = createVector(w*0.001, h*0.001);  // make the vector a multiple of width & height
 }
 
@@ -32,16 +32,16 @@ Physics.prototype.constantForce = function() {
 
 // * Apply a Force * //
 Physics.prototype.applyForce = function(force, bool=true) {
-  var pos_vec = createVector(0, 0);
+	var pos_vec = createVector(0, 0);
 	var a = force.div(this.mass);
-  this.acc.add(a);
+	this.acc.add(a);
 	this.vel.add(this.acc);
-  pos_vec.add(this.vel);
+	pos_vec.add(this.vel);
 	pos_vec.x *= this.ratio.x;
 	pos_vec.y *= this.ratio.y;
 	if (bool) { this.pos.add(pos_vec); }
 	if (!bool) { this.pos.x += pos_vec.x; }
-  this.acc.set(0, 0);
+	this.acc.set(0, 0);
 }
 
 // * Remove All Forces * //
@@ -61,21 +61,21 @@ Physics.prototype.moveX = function(x) {
 
 // * Gravity * //
 Physics.prototype.gravity = function(s=1) {
-	var gravity = createVector(0, this.mass * this.g * (this.pos.y/h) * s);
+	var gravity = createVector(0, this.mass * this.g * this.pos.y * s);
 	this.applyForce(gravity);
 }
 
 // * Normal Force * //
 Physics.prototype.normalForce = function() {
-  if (this.pos.y < 0) {
-    this.vel.y *= 0;
-    this.pos.y = 0;
-  }
+	if (this.pos.y < 0) {
+		this.vel.y *= 0;
+		this.pos.y = 0;
+	}
 }
 
 // * Friction * //
 Physics.prototype.friction = function() {
-  if ((!keyIsDown(LEFT_ARROW)) && (!(keyIsDown(RIGHT_ARROW)) && (!midair))) {
+	if ((!keyIsDown(LEFT_ARROW)) && (!(keyIsDown(RIGHT_ARROW)) && (!midair))) {
 		var Ff = createVector(0, 0);
 		Ff.x = (-this.mu) * this.vel.x;
 		this.applyForce(Ff);
